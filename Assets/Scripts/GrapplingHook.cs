b4bf14551736f7de3a9be167744bc525860c230f
Joseph Rossi <joethered@gmail.com>
2015-08-13 02:33:01 -0700

    Improved movement.  Diagonal Jumping is no longer possible.  Character still gets stuck often when walking on a surface. Jump now has a button that will be shared with Grapple (currently the 'F' key).  Some initial setup for the Grappling Hook.

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GrapplingHook : MonoBehaviour {
+	
+	public GameObject chainLink;
+	List<GameObject> links;
+	bool latched = false;
+	public LayerMask whatIsGround;
+	Rigidbody2D rb2D;
+	int extend = 0;
+	public float maxLength = 1f;
+	float length = 0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+
 	}
+
+	public void setVelocity(float xVel, float yVel){
+		rb2D.velocity = new Vector2 (xVel, yVel);
+	}
+
+	public Vector2 getVelocity(){
+		return rb2D.velocity;
+	}
+
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
+
+
+	public GameObject grapplingHook;
 	public float maxSpeed = 1f;
 	public int rot = 0;
 	public bool grounded = false;
@@ -11,6 +16,7 @@ public class Player : MonoBehaviour {
 	public Transform leftCheck;
 	public Transform rightCheck;
 	public float groundRadius = 0.1f;
+
 
 	public bool flipped = false;
 
@@ -32,76 +38,56 @@ public class Player : MonoBehaviour {
 		bool upCol = Physics2D.OverlapCircle (upCheck.position, groundRadius, whatIsGround);
 		bool leftCol = Physics2D.OverlapCircle (leftCheck.position, groundRadius, whatIsGround);
 		bool rightCol = Physics2D.OverlapCircle (rightCheck.position, groundRadius, whatIsGround);
+		Debug.Log (downCol.ToString() + upCol.ToString() + leftCol.ToString() + rightCol.ToString());
+
 
 		bool nextGroundState = (downCol || upCol || leftCol || rightCol);
 
-		if (!grounded) {
-			if (nextGroundState && !downCol) {
-				if (upCol) {
-					switch (rot){
-					case 0:
-						rot = 180;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						rb2D.velocity = new Vector2 (rb2D.velocity.x, 1f);
-						break;
-					case 180:
-						rot = 0;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						rb2D.velocity = new Vector2 (rb2D.velocity.x, -1f);
-						break;
-					case 90:
-						rot = 270;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						rb2D.velocity = new Vector2 (-1f, rb2D.velocity.x);
-						break;
-					case 270:
-						rot = 90;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						rb2D.velocity = new Vector2 (1f, rb2D.velocity.x);
-						break;
-					}
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
 
-				}else if (leftCol){
-					switch (rot){
-					case 0:
-						rot = 270;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 180:
-						rot = 90;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 90:
-						rot = 0;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 270:
-						rot = 180;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					}
-				}else if (rightCol){
-					switch (rot){
-					case 0:
-						rot = 90;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 180:
-						rot = 270;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 90:
-						rot = 180;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					case 270:
-						rot = 0;
-						transform.eulerAngles = new Vector3(0,0,rot);
-						break;
-					}
-				}
+		if (!grounded) {
+			if (inputX < 0){
+				rot = 180;
+				transform.eulerAngles = new Vector3(0,0,rot);
 			}
+			if (inputX > 0){
+				rot = 0;
+				transform.eulerAngles = new Vector3(0,0,rot);
+			}
+			if (inputY < 0){
+				rot = 270;
+				transform.eulerAngles = new Vector3(0,0,rot);
+			}
+			if (inputY > 0){
+				rot = 90;
+				transform.eulerAngles = new Vector3(0,0,rot);
+			}
+
+
+			if (nextGroundState && !downCol) {
+				latchToWall(upCol, leftCol, rightCol);
+			}
+
+			downCol = Physics2D.OverlapCircle (downCheck.position, groundRadius, whatIsGround);
+			nextGroundState = (upCol || leftCol || rightCol);
+			if (!downCol && nextGroundState){
+				latchToWall(upCol, leftCol, rightCol);
+			}
+
+
+
+			if (Input.GetAxis("AButton") > 0 && GameObject.Find("GrapplingHook") != null){
+				GameObject toInstantiate = grapplingHook;
+				GameObject instance = Instantiate(toInstantiate, new Vector3(transform.position.x, transform.position.y, 0f), Quaternion.identity) as GameObject;
+				GrapplingHook gHook = instance.GetComponent<GrapplingHook>();
+				gHook.setVelocity(rb2D.velocity.x + 1, rb2D.velocity.y);
+			}
+
+
 		}
+
+
 		if (nextGroundState != grounded) {
 			if (!grounded){
 				grounded = true;
@@ -109,15 +95,31 @@ public class Player : MonoBehaviour {
 		}
 
 
+		grounded = nextGroundState;
 
-
-		float inputX = Input.GetAxis ("Horizontal");
-		float inputY = Input.GetAxis ("Vertical");
-		Debug.Log (rb2D.velocity);
 		if (grounded) {
-			rb2D.velocity = new Vector2 (inputX * maxSpeed, rb2D.velocity.y);
-			rb2D.velocity = new Vector2 (rb2D.velocity.x, inputY * maxSpeed);
-			grounded = false;
+			switch (rot){
+			case 0:
+				rb2D.velocity = new Vector2 (inputX * maxSpeed, rb2D.velocity.y);
+				if (Input.GetAxis("AButton") > 0)
+					rb2D.velocity = new Vector2 (0, 0.5f);
+				break;
+			case 180:
+				rb2D.velocity = new Vector2 (inputX * maxSpeed, rb2D.velocity.y);
+				if (Input.GetAxis("AButton") > 0)
+					rb2D.velocity = new Vector2 (0, -0.5f);
+				break;
+			case 90:
+				rb2D.velocity = new Vector2 (rb2D.velocity.x, inputY * maxSpeed);
+				if (Input.GetAxis("AButton") > 0)
+					rb2D.velocity = new Vector2 (-0.5f, 0);
+				break;
+			case 270:
+				rb2D.velocity = new Vector2 (rb2D.velocity.x, inputY * maxSpeed);
+				if (Input.GetAxis("AButton") > 0)
+					rb2D.velocity = new Vector2 (0.5f, 0);
+				break;
+			}
 		}
 	}
 
@@ -125,6 +127,73 @@ public class Player : MonoBehaviour {
 
 	}
 
-
-
+	private void latchToWall(bool upCol, bool leftCol, bool rightCol){
+		if (upCol) {
+			switch (rot){
+			case 0:
+				rot = 180;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				rb2D.velocity = new Vector2 (rb2D.velocity.x, 1f);
+				break;
+			case 180:
+				rot = 0;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				rb2D.velocity = new Vector2 (rb2D.velocity.x, -1f);
+				break;
+			case 90:
+				rot = 270;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				rb2D.velocity = new Vector2 (-1f, rb2D.velocity.x);
+				break;
+			case 270:
+				rot = 90;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				rb2D.velocity = new Vector2 (1f, rb2D.velocity.x);
+				break;
+			}
+			
+		}else if (leftCol){
+			switch (rot){
+			case 0:
+				rot = 270;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 180:
+				rot = 90;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 90:
+				rot = 0;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 270:
+				rot = 180;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			}
+		}else if (rightCol){
+			switch (rot){
+			case 0:
+				rot = 90;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 180:
+				rot = 270;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 90:
+				rot = 180;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			case 270:
+				rot = 0;
+				transform.eulerAngles = new Vector3(0,0,rot);
+				break;
+			}
+		}
+		
+	}
+	
+	
+	
 }
